3ed354caf0bbebbd85fdc818a4ce1de54a8b9f8d
alfredijiga <alfredijiga1@gmail.com>
2022-09-15 15:02:18 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * print_number - Prints a number
- * @n: The number to print
- */
+** print_number - Prints a number
+* @n: The number to print
+*/
 
 void print_number(int n)
 {
